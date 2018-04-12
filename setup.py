58c1e5331f5094e9d0cda880fefb28c00658fe28
David Salisbury <git@david-salisbury.co.uk>
2018-04-12 15:30:00 +0100

    Create basic package structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='safecamera-decryptor',
+    description='Python-based decryptor for files encrypted with SafeCamera',
+    license='MIT',
+    url='https://github.com/dsalisbury/safecamera-decryptor',
+
+    version='0.0.1',
+    packages=find_packages(),
+
+    author='Dave Salisbury',
+    author_email='safecamera-decryptor@dsalisbury.to',
+
+    install_requires=(
+        'cryptography',
+        'jasypt4py',
+        'pycrypto',
+    ),
+)
