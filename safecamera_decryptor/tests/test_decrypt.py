98c13fbdc7f97cde96322a24c88c9fca4bec3136
David Salisbury <git@david-salisbury.co.uk>
2018-04-17 11:01:40 +0100

    Add some tests for the content provider

--- safecamera_decryptor/tests/test_decrypt.py
+++ safecamera_decryptor/tests/test_decrypt.py
@@ -1,7 +1,11 @@
+from contextlib import contextmanager
+from tempfile import NamedTemporaryFile
+
 import pytest
 
 from safecamera_decryptor.decrypt import (
-    CipherParams, CorruptOrInvalidPassword, Decryptor, FilenameProvider)
+    CipherParams, CorruptOrInvalidPassword, Decryptor, FilenameProvider,
+    FileContentProvider)
 
 
 def join(seq):
@@ -51,3 +55,35 @@ def test_filename_provider_unhappy_paths():
 
     with pytest.raises(ValueError):
         FilenameProvider(path='zzSC-1-NotARealHexDump.sc').get_params()
+
+
+@contextmanager
+def temporary_content(content):
+    with NamedTemporaryFile() as temp:
+        temp.write(content)
+        temp.flush()
+        yield temp.name
+
+
+def test_content_provider_happy_path():
+    salt = b'facecafe' * 4
+    iv = b'0123456789abcdef'
+    payload = b'1234567890'
+    with temporary_content(b'SCAES1' + iv + salt + payload) as path:
+        prov = FileContentProvider(path=path)
+        assert prov.get_params() == CipherParams(salt=salt, iv=iv)
+        assert join(prov.read()) == b'1234567890'
+
+
+def test_content_provider_bad_magic_string():
+    with temporary_content(b'SOCKS12354523423') as path:
+        prov = FileContentProvider(path=path)
+        with pytest.raises(ValueError, match=r'Invalid magic string.+SOCKS'):
+            prov.get_params()
+
+
+def test_content_provider_bad_version_number():
+    with temporary_content(b'SCAES2123556969') as path:
+        prov = FileContentProvider(path=path)
+        with pytest.raises(ValueError, match=r'Invalid version.+2'):
+            prov.get_params()
